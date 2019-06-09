7704f078bb1fa6b5f6adc2b5240bd2c08d00d1ea
Alexandru Fatu <alexandru.fatu@hotmail.com>
2019-06-09 14:44:07 +0300

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace GradeBook.GradeBooks {
     public class RankedGradeBook : BaseGradeBook {
 
-        public RankedGradeBook(string name) : base(name) {
+        public RankedGradeBook(string name, bool isWeighted) : base(name, isWeighted) {
             Type = GradeBookType.Ranked;
         }
 
